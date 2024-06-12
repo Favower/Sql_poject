c97c9821c9ab5e426f14bbdd7c22ed21cc96456d
Favower <107644108+Favower@users.noreply.github.com>
2024-06-12 04:15:00 +0300

    -
    -

---
+++ sql_course.session.sql
@@ -0,0 +1,7 @@
+SELECT 
+count(job_id) as job_posted_count,
+EXTRACT (MONTH FROM job_posted_date) as date
+FROM job_postings_fact
+WHERE job_title = 'Data Analyst'
+GROUP BY date
+ORDER BY job_posted_count DESC;

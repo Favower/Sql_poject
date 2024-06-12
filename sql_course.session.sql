7dfe4097638b80333c59cd9920e2e7c75b0304fc
Favower <Amir.umakhanov@yandex.ru>
2024-06-12 18:33:50 +0300

    Work on new feature

--- sql_course.session.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-count(job_id) as job_posted_count,
-EXTRACT (MONTH FROM job_posted_date) as date
-FROM job_postings_fact
-WHERE job_title = 'Data Analyst'
-GROUP BY date
-ORDER BY job_posted_count ASC;
